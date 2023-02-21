f75cb359393d08da8f04b3910cc4db35c774fcc9
hyper-velocity <hypervelocity3@gmail.com>
2023-02-21 09:46:57 -0500

    feat(layout): build default layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-neutral-800">
+      <q-toolbar class="text-lg font-semibold">
+        Vouch Test
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="p-3">
+    <user-list :users="users" />
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { users } from 'src/const'
+</script>
+
+<route>
+{
+  "name": "Home"
+}
+</route>
